a172ca9760dab579792f0cc2998b927a018b2fdd
David Dimande <davidtimoteodimande@gmail.com>
2025-05-18 20:42:04 +0100

    commented provider settings

--- main.tf
+++ main.tf
@@ -1,13 +1,12 @@
 terraform {
   required_version = "~> 1.2"
- /* required_providers {
+  required_providers {
     proxmox = {
       source  = "Telmate/proxmox"
       version = "3.0.1-rc9"
     }
 
   }
-  */
 
 }
 
